80f26ec74dd23e1222e3a1a17d3634f464de4d09
Mohammadreza Nikfal <mohammadreza.nikfal@bencorinc.com>
2020-07-24 08:42:49 -0500

    Add Material & Setup SignUp page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { MaterialModule } from "../material/material.module";
 
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+const MaterialComponents = [
+  MatFormFieldModule, MatInputModule
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
