4d8849619ef7c86ef8d50638e6c26d30806abc38
Mohammadreza Nikfal <mohammadreza.nikfal@bencorinc.com>
2020-07-24 08:55:54 -0500

    Add Submit button to SignUp form

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 
 const MaterialComponents = [
-  MatFormFieldModule, MatInputModule
+  MatFormFieldModule, MatInputModule,MatButtonModule
 ];
 
 
