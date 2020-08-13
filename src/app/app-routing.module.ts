2bf765b8d15cbc7a54407c7171c3affefcb917eb
Mohammadreza Nikfal <mohammadreza.nikfal@bencorinc.com>
2020-08-13 10:14:23 -0500

    Implement GUARD (skipWhile/take/tap)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuard } from "./auth/auth.guard";
-import { AuthModule } from './auth/auth.module';
 
 const routes: Routes = [
-  { path: 'inbox', canLoad: [AuthModule], loadChildren: () => import('./inbox/inbox.module').then(mod => mod.InboxModule) }
+  { path: 'inbox', canLoad: [AuthGuard], loadChildren: () => import('./inbox/inbox.module').then(mod => mod.InboxModule) }
 ];
 
 @NgModule({
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { skipWhile, take, tap } from 'rxjs/operators';
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanLoad {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
+export class AuthGuard implements CanLoad {
+
+  constructor(private authService: AuthService, private router: Router) { }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    return this.authService.signedin$.pipe(
+      skipWhile(value => value === null),
+      take(1),
+      tap((authenticated) => {
+        if (!authenticated) {
+          this.router.navigateByUrl('/')
+        }
+      })
+    );
   }
 }
